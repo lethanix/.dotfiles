553225114c853a525745f4d189d18cb1ee2540ba
lethanix <louismurguia@protonmail.com>
2021-12-04 13:40:16 -0600

    Space between {{}}, conflict with dotter templates

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,7 +36,7 @@ return require('packer').startup(function()
 
     use {
       'nvim-telescope/telescope.nvim',
-      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+      requires = { {'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'} }
     }
 
     use {
