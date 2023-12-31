9d5342f1aa7749b031c176fc471cd65193fa6c7b
Louis Murguia <lethani@192.168.68.63>
2023-12-31 00:33:27 -0600

    Add neovim configuration in home manager

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, shared, ... }: {
+  imports = [
+    ./shared/nvim/neovim.nix
+    #./dotnix/git.nix
+    #./dotnix/bash.nix
+    #./dotnix/tmux.nix
+  ];
   # This is required information for home-manager to do its job
   home = {
     stateVersion = "23.11";
@@ -14,5 +20,32 @@
 
   # I use fish, but bash and zsh work just as well here. This will setup
   # the shell to use home-manager properly on startup, neat!
-  # programs.fish.enable = true;
+  programs.bash.enable = true;
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+
+    # Alacritty config folder from GitHub repository
+    # ".config/alacritty".recursive = true;
+    # ".config/alacritty".source = ./dotany/alacritty;
+ 
+    ".shared" = { source = ./shared; recursive = true; };
+    # Neovim config folder
+    ".config/nvim".recursive = true;
+    ".config/nvim".source = ../shared/nvim;
+
+    # ".config/starship.toml".source = ./dotany/starship.toml;
+    # ".config/tmux.conf".source = ./dotany/tmux.conf;
+  };
 }
