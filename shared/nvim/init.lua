0f721eecf74941d8b74fc3fcad4aea5c1b21016d
lethanix <louismurguia@lethanix.com>
2024-02-01 23:41:50 -0600

    Change to lazy nvim

--- shared/nvim/init.lua
+++ shared/nvim/init.lua
@@ -1,29 +1,42 @@
-if  vim.g.vscode then
-    require("settings") -- Load general settings
-    require("comments")
-    require("mappings") -- Load general mappings
-else
-    require("settings") -- Load general settings
-    require("mappings") -- Load general mappings
-    -- require("plugins") -- Load plugins
-    require("colorscheme_conf") -- Load nvim theme and colors
-
-    -- Load plugins settings
-    require("null")
-    -- require("indent")
-    require("comments")
-    require("dashboard")
-    require("treesitter")
-    require("statusline")
-    require("autopairs-conf")
-    require('telescope-config')
-
-    -- Load LSP settings
-    require("lsp-servers")
-
-    if vim.g.neovide then
-        require("neovide_conf")
-    end
-
-    require("preview-colors") -- Load color highlighter NOTE: add this line after/below where your plugins are setup.
+-- ****************************************************************
+-- Bootstrap lazy
+-- ****************************************************************
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+
+-- ****************************************************************
+-- ****************************************************************
+
+require("settings") -- Load general settings
+require("mappings") -- Load general mappings
+require("lazy").setup("plugins") -- Load plugins
+require("colorscheme_conf") -- Load nvim theme and colors
+
+-- Load plugins settings
+require("null")
+-- require("indent")
+require("comments")
+require("dashboard")
+require("treesitter")
+require("statusline")
+require("autopairs-conf")
+require('telescope-config')
+
+-- Load LSP settings
+require("lsp-servers")
+
+if vim.g.neovide then
+require("neovide_conf")
+end
+
+require("preview-colors") -- Load color highlighter NOTE: add this line after/below where your plugins are setup.
