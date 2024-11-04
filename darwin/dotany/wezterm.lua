4cd53a4fd4a0aad96268d983e8d6383a39f57b81
lethanix <louismurguia@proton.me>
2024-11-03 21:54:49 -0600

    Update WezTerm config

--- darwin/dotany/wezterm.lua
+++ darwin/dotany/wezterm.lua
@@ -4,10 +4,37 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+--**********************************************
+-- Colorscheme configuration
+--**********************************************
+-- Change the color scheme depending of the system preference:
+function scheme_for_appearance(appearance)
+    if appearance:find "Dark" then
+        return 'Tokyo Night'
+        -- return "Catppuccin Mocha"
+    else
+        return 'Tokyo Night Day'
+        -- return "Catppuccin Latte"
+    end
+end
 
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+--**********************************************
+-- Font configuration
+--**********************************************
+-- config.font = wezterm.font 'SpaceMono Nerd Font'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 17
+
+--**********************************************
+-- Tab bar and window decorations configuration
+--**********************************************
+config.enable_tab_bar = false
+-- config.use_fancy_tab_bar = false
+-- config.tab_bar_at_bottom = true
+
+config.window_decorations = "RESIZE"
 
 -- and finally, return the configuration to wezterm
 return config
