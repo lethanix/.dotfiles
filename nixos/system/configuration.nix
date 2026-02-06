34d16cec32620cfdb97a22e124b7ed8c1314f989
lethanix <me@lethanix.com>
2026-02-05 23:58:52 -0600

    Add power management

--- nixos/system/configuration.nix
+++ nixos/system/configuration.nix
@@ -45,7 +45,31 @@
     xkb.layout = "us";
     xkb.variant = "";
   };
+  
+  # Configure power management
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 50;
+
+       #Optional helps save long term battery health
+       START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+       
+       START_CHARGE_THRESH_BAT1=40;
+       STOP_CHARGE_THRESH_BAT1=80;
+      };
+  };
+  
   # **********************************************************************
 
   users.users.lethanix = {
