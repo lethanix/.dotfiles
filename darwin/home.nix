e5c4b4fa2941ec2ff53e400c36a3a7af3c2111ed
lethanix <me@lethanix.com>
2026-04-18 17:13:29 -0400

    Cleanup

--- darwin/home.nix
+++ darwin/home.nix
@@ -7,28 +7,15 @@
     # ../shared/tmux/tmux.nix
   ];
 
-  # This is required information for home-manager to do its job
   home = {
     stateVersion = "24.05";
     username = "lethani";
     homeDirectory = "/Users/lethani";
     packages = with pkgs; [
-
-      # neovim => this will be enabled when using the option programs.neovim.enable = true in the neovim.nix file
-
       # Using better bash (not macOS/Apple)
       # bash
       # direnv
       # bashInteractive
-
-      # alacritty
-      # nodejs
-      rustup
-      # rustc
-      # cargo
-      # gnumake
-      # cmake
-      # go
 
       # DevOps
       # kubectl => use asdf instead in order to avoid version conflicts with K8s API
@@ -42,6 +29,7 @@
       zoxide
       ffmpeg
       bottom
+      rustup
 
       tmux
       glow
@@ -50,8 +38,6 @@
       fzf 
       fd
       gh
-
-      # unzip
 
       # macOS
       # iina
@@ -64,7 +50,6 @@
       nerd-fonts.fira-code 
       nerd-fonts.jetbrains-mono
       nerd-fonts.space-mono
-      # nerd-fonts.commit-mono
       commit-mono
     ];
   };
@@ -74,28 +59,11 @@
   # /Users/<username>/Library/whatever
   xdg.enable = true;
 
-  # I use fish, but bash and zsh work just as well here. This will setup
-  # the shell to use home-manager properly on startup, neat!
-  programs.bash.enable = true;
+  # This will setup the shell to use home-manager properly on startup
+  # programs.bash.enable = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # Dotfiles management
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
-    # # Alacritty config folder from GitHub repository
-    # ".config/alacritty".recursive = true;
-    # ".config/alacritty".source = ../shared/alacritty;
-
     # Neovim config folder
     ".config/nvim".recursive = true;
     ".config/nvim".source = ../shared/nvim;
