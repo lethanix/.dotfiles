5a480fa1c1fe07b49220929444167e7dcc7cf1aa
lethanix <me@lethanix.com>
2026-05-02 21:31:14 -0400

    NixOS: Add nix formatter

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,29 +1,35 @@
 {
-	description = "NixOS to GTD";
-	inputs = {
-		nixpkgs.url = "nixpkgs/nixos-25.11";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-		vicinae.url = "github:vicinaehq/vicinae";
-		vicinae-extensions.url = "github:vicinaehq/extensions";
-	};
+  description = "NixOS to GTD";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    vicinae.url = "github:vicinaehq/vicinae";
+    vicinae-extensions.url = "github:vicinaehq/extensions";
+  };
 
-	outputs = { self, nixpkgs, home-manager, ...} @ inputs: {
-		nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-			specialArgs = { inherit inputs; };
-			modules = [
-				./system/configuration.nix
-				home-manager.nixosModules.home-manager {
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.lethanix = import ./users/lethanix/home.nix;
-						backupFileExtension = "bak";
-					};
-				}
-			];
-		};
-	};
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
+      nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./system/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.lethanix = import ./users/lethanix/home.nix;
+              backupFileExtension = "bak";
+            };
+          }
+        ];
+      };
+    };
 }
